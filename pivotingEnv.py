a031c016ef7793ad969f788a48485198a98ef74e
Kodex <leotoledo00@gmail.com>
2020-01-29 12:44:27 -0300

    Simulação finalmente funcionando. Ela está funcionando com o PPO e o pivotingEnv está configurado para interagir com ele

--- pivotingEnv.py
+++ pivotingEnv.py
@@ -4,49 +4,78 @@ from gym.envs.mujoco import mujoco_env
 
 class InvertedPendulumEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
+        ##################################################################
+        self.desired_angle = 30
+        self.counter = 0
+        self.new_angle_counter = 0
+        ##################################################################
+        
         utils.EzPickle.__init__(self)
         mujoco_env.MujocoEnv.__init__(self, 'pivoting.xml', 2)
-
+    
     def step(self, a):
+        self.new_angle_counter += 1
+        if(self.new_angle_counter > 4000000):
+            self.desired_angle = 45
+        elif(self.new_angle_counter > 8000000):
+            self.desired_angle = 15
         self.do_simulation(a, self.frame_skip)
-        ob = self._get_obs()
+        ob = self._get_obs()    
         
     #Modelando a reward
-        #Enquanto não cair, mas não completar o objetivo, a reward é -1
-        if(ob[4] > 0.07): 
-            reward = -1
-        #Quando cair, a reward é -1
-        elif(ob[4] <= 0.07):
-            reward = -2
-        #Caso atinja o ângulo desejado, a reward deve ser modelada no código principal
-        #Modelando se done é True ou não. Quando o objeto cair, done==1. O caso de limite de tempo deve ser modelado no código principal
-        done = 0
-        if(reward == -2):
+        #Caso complete o objetivo
+        if(ob[0] >= self.desired_angle - 0.5 and ob[0] <= self.desired_angle + 0.5): 
+            #O denominador da reward busca aumentar a precisão, enquanto o numerador aliado aos ifs 
+            reward = (-1)*np.abs(ob[0] - self.desired_angle)/180
+            '''
+            #busca aumentar o tempo de permanência no ângulo desejado
+            if(self.counter >= 0 and self.counter <= 20):
+                reward = (-1)*np.abs(ob[0] - self.desired_angle)/10
+            if(self.counter > 20 and self.counter <=60):
+                reward = (-1)*np.abs(ob[0] - self.desired_angle)/20
+            '''
+            
+            self.counter = self.counter + 1
             done = 0
-        
+            if(self.counter > 60):
+                reward = 100
+                print("******************Completou********************")
+                done = 1
+                self.counter = 0
+                return ob, reward, done, {}
+        #Se não completar
+        else:
+            self.counter = 0
+            reward = (-1)*np.abs(ob[0] - self.desired_angle)/180
+            done = 0
+        #print("REWARD: ", reward)
+        ####
         return ob, reward, done, {}
 
     def reset_model(self):
+        #Recebe qpos e qvel do modelo do mujoco, com vários dados que não serão utilizados
         qpos = self.init_qpos
         qvel = self.init_qvel
+       
         self.set_state(qpos, qvel)
         return self._get_obs()
 
     def _get_obs(self):
         obs = np.concatenate([self.sim.data.qpos]).ravel()
         #Vamos converter o angulo da ferramenta para graus
-#        conv_rad = obs[5]
         conv_rad = np.arctan2(2*(obs[5]*obs[8] + obs[6]*obs[7]), (1 - 2*(obs[7]**2 + obs[8]**2)))
         obs_degrees = 180*conv_rad/np.pi
         
         obs[5] = obs_degrees
+        #print("ANGULO DA FERRAMENTA: ", obs[5])
         
-        #Convertendo o Hinge do Arm para Graus
+        #Convertendo o Angulo do Gripper para Graus
         obs[0] = 180*obs[0]/np.pi
+        #print("ANGULO DO GRIPPER: ", obs[0])
         
         #obs[0] será o ângulo relativo em graus
-#        obs[0] = obs[5] - obs[0]
-        
+        obs[0] = obs[5] - obs[0]
+        #print("ANGULO RELATIVO: ", obs[0])
         return obs
 
     def viewer_setup(self):
@@ -69,3 +98,4 @@ class InvertedPendulumEnv(mujoco_env.MujocoEnv, utils.EzPickle):
 ###########################
 #Para achar o ângulo da ferramenta relativamente ao braço, faça obs[5]-obs[0]
 #Para saber se a ferramenta caiu, verificar de obs[4] > 0.07 (Não caiu)
+
